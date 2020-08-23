501d5d2052107dbc2c878a58713133baf51d9f6c
Mikhail Tikhonov <46346390+MikhailTikhonovA@users.noreply.github.com>
2020-08-23 13:55:54 +0300

    upd

--- src/test/java/tests/AmazonTests.java
+++ src/test/java/tests/AmazonTests.java
@@ -7,8 +7,11 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
+
+@Tag("AmazonCartTests")
 public class AmazonTests {
 
+    @Tag("UserAmazonCartTest")
     @Test
     public void testEmptyCartUnregisterUser() {
         open("https://www.amazon.com");
@@ -16,6 +19,14 @@ public class AmazonTests {
         $("#nav-cart").click();
 
         $(".a-row.sc-your-amazon-cart-is-empty").shouldHave(text("Your Amazon Cart is empty"));
+    }
+
+    @Tag("AmazonCartTest")
+    @Test
+    public void testCartUnregisterUser() {
+        open("https://www.amazon.com");
+
+        $("#nav-cart").shouldHave(text("Cart"));
     }
 
 }
